64fd97a5aa794531159713689d4fb7d6193e6f54
cruzerblade95 <nabilajwad10@gmail.com>
2025-02-27 17:44:47 +0800

    new update

--- backend/db_choosedate.php
+++ backend/db_choosedate.php
@@ -5,8 +5,9 @@ include('connection.php');
 if (!isset($_SESSION['user_id'])) {
     die("Error: User not logged in."); // Debug message
 }
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+echo($_SERVER['REQUEST_METHOD']."<br/>");
+print_r($_POST);
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['date'])) {
     $user_id = $_SESSION['user_id']; // Now it should be set
     $date = $_POST['date'];
     $time = $_POST['time'];
@@ -19,12 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     $ic_cadangan2 = $_POST['ic_cadangan2'];
     $phone_cadangan2 = $_POST['phone_cadangan2'];
     $email_cadangan2 = $_POST['email_cadangan2'];
-    $status_code = 'pending';
+    $status_code = 0;
+    $tindakan_code = 0;
+    $comment = "";
     $masjid_id = $_SESSION['masjid_id'];
 
     try {
-        $stmt = $conn->prepare("INSERT INTO booking (user_id, date, time, place, nama_cadangan1, ic_cadangan1, phone_cadangan1, email_cadangan1, nama_cadangan2, ic_cadangan2, phone_cadangan2, email_cadangan2, status_code, masjid_id) 
-                                VALUES (:user_id, :date, :time, :place, :nama_cadangan1, :ic_cadangan1, :phone_cadangan1, :email_cadangan1, :nama_cadangan2, :ic_cadangan2, :phone_cadangan2, :email_cadangan2, :status_code, :masjid_id)");
+        $stmt = $conn->prepare("INSERT INTO booking (user_id, date, time, place, nama_cadangan1, ic_cadangan1, phone_cadangan1, email_cadangan1, nama_cadangan2, ic_cadangan2, phone_cadangan2, email_cadangan2, status_code, tindakan_code, comment, masjid_id) 
+                                VALUES (:user_id, :date, :time, :place, :nama_cadangan1, :ic_cadangan1, :phone_cadangan1, :email_cadangan1, :nama_cadangan2, :ic_cadangan2, :phone_cadangan2, :email_cadangan2, :status_code, :tindakan_code, :comment, :masjid_id)");
         $stmt->bindParam(':user_id', $user_id);
         $stmt->bindParam(':date', $date);
         $stmt->bindParam(':time', $time);
@@ -38,6 +41,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
         $stmt->bindParam(':phone_cadangan2', $phone_cadangan2);
         $stmt->bindParam(':email_cadangan2', $email_cadangan2);
         $stmt->bindParam(':status_code', $status_code);
+        $stmt->bindParam(':tindakan_code', $tindakan_code);
+        $stmt->bindParam(':comment', $comment);
         $stmt->bindParam(':masjid_id', $masjid_id);
         
         $stmt->execute();
